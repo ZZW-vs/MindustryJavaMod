04980e26899efde828653003073adeca347d4965
ZZW-vs <Zzw13675816057@outlook.com>
2025-11-01 01:58:07 +0800

    b

---
+++ src/Text/TextMod.java
@@ -0,0 +1,25 @@
+package Text;
+
+import arc.Events;
+import arc.util.Time;
+import mindustry.game.EventType;
+import mindustry.mod.Mod;
+import mindustry.ui.dialogs.BaseDialog;
+
+public class TextMod extends Mod {
+    public TextMod() {
+        Events.on(EventType.ClientLoadEvent.class, e -> {
+            Time.run(10f, () -> {
+                BaseDialog  dialog = new BaseDialog("Welcome to use my mod!");
+                dialog.cont.add("I am ZZW, Thank you for using my mod!");
+                Time.run(100f, dialog::addCloseButton);
+                dialog.show();
+            });
+        });
+    }
+
+    @Override
+    public void loadContent() {
+        super.loadContent();
+    }
+}
