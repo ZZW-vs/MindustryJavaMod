8f4cd5132337ab035da433d65d354bbf09b55b60
ZZW-vs <Zzw13675816057@outlook.com>
2025-11-02 12:17:13 +0800

    f

--- src/zzw/content/blocks.java
+++ src/zzw/content/blocks.java
@@ -7,15 +7,23 @@ import mindustry.type.ItemStack;
 import mindustry.world.Block;
 import mindustry.world.blocks.production.GenericCrafter;
 
-import static mindustry.world.meta.StatUnit.items;
-import static zzw.content.items.Iron;
-import static zzw.content.items.Iron_Plate;
-
 public class blocks {
     public static Block Furnace, Processing_Factory;
     public static void load(){
         Furnace = new GenericCrafter("furnace"){{
-            requirements(Category.crafting, ItemStack.with(Items.copper, 100, Items.lead, 80,  Items.Iron, 30));
+            requirements(Category.crafting, ItemStack.with(Items.copper, 100, Items.lead, 80, items.Iron, 30));
+            alwaysUnlocked = true;
+            craftEffect = Fx.pulverizeMedium;
+            outputItem = new ItemStack(items.Iron_Plate, 1);
+            consumeItem(zzw.content.items.Iron, 1);
+
+            size = 2;
+            hasItems = true;
+            craftTime = 90;
+        }};
+
+        Processing_Factory = new GenericCrafter("processing_factory"){{
+            requirements(Category.crafting, ItemStack.with(items.Iron, 100, Items.lead, 80, items.Iron_Plate, 30));
             alwaysUnlocked = true;
             craftEffect = Fx.pulverizeMedium;
             outputItem = new ItemStack(items.Iron_Plate, 1);
@@ -24,10 +32,6 @@ public class blocks {
             size = 2;
             hasItems = true;
             craftTime = 90;
-        }};
-
-        Processing_Factory = new GenericCrafter("processing_factory"){{
-
         }};
 
     }
