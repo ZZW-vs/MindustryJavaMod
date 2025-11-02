127cf1c86ccfcda84ace84e03e11b656a92f9f7c
ZZW-vs <Zzw13675816057@outlook.com>
2025-11-03 00:52:40 +0800

    k

--- src/zzw/content/blocks.java
+++ src/zzw/content/blocks.java
@@ -8,31 +8,59 @@ import mindustry.world.Block;
 import mindustry.world.blocks.production.GenericCrafter;
 
 public class blocks {
-    public static Block  Plate_Maker, Large_Plate_Maker;
+    public static Block Plate_Maker_Iron, Large_Plate_Maker_Iron, Plate_Maker_Gold, Large_Plate_Maker_Gold;
     public static void load(){
-        Plate_Maker = new GenericCrafter("plate_maker"){{
+        Plate_Maker_Iron = new GenericCrafter("plate_maker_iron"){{
             requirements(Category.crafting, ItemStack.with(Items.copper, 90, Items.lead, 70, items.Iron, 35));
 
             alwaysUnlocked = true;
             craftEffect = Fx.pulverizeMedium;
+
             outputItem = new ItemStack(items.Iron_Sheet, 2);
             consumeItem(items.Iron, 2);
 
             size = 2;
             hasItems = true;
-            craftTime = 65f;
+            craftTime = 70f;
         }};
 
-        Large_Plate_Maker = new GenericCrafter("large_plate_maker"){{
+        Large_Plate_Maker_Iron = new GenericCrafter("large_plate_maker_iron"){{
             requirements(Category.crafting, ItemStack.with(items.Iron, 130, Items.lead, 100, items.Iron, 40, items.Iron_Sheet, 25));
             alwaysUnlocked = true;
             craftEffect = Fx.pulverizeMedium;
+
             outputItem = new ItemStack(items.Iron_Sheet, 4);
             consumeItem(items.Iron, 3);
 
             size = 3;
             hasItems = true;
-            craftTime = 55f;
+            craftTime = 60f;
+        }};
+
+        Plate_Maker_Gold = new GenericCrafter("plate_maker_gold"){{
+            requirements(Category.crafting, ItemStack.with(Items.copper, 95, Items.lead, 75, items.Gold, 30));
+            alwaysUnlocked = true;
+            craftEffect = Fx.pulverizeMedium;
+
+            outputItem = new ItemStack(items.Gold_Sheet, 2);
+            consumeItem(items.Gold, 2);
+
+            size = 2;
+            hasItems = true;
+            craftTime = 80f;
+        }};
+
+        Large_Plate_Maker_Gold = new GenericCrafter("large_plate_maker_gold"){{
+            requirements(Category.crafting, ItemStack.with(items.Iron, 140, Items.lead, 110, items.Gold, 40, items.Gold_Sheet, 25));
+            alwaysUnlocked = true;
+            craftEffect = Fx.pulverizeMedium;
+
+            outputItem = new ItemStack(items.Gold_Sheet, 4);
+            consumeItem(items.Gold, 3);
+
+            size = 3;
+            hasItems = true;
+            craftTime = 70f;
         }};
 
     }
--- src/zzw/content/items.java
+++ src/zzw/content/items.java
@@ -3,16 +3,26 @@ package zzw.content;
 import mindustry.type.Item;
 
 public class items {
-    public static Item Iron, Iron_Sheet, Nails;
+    public static Item Iron, Iron_Sheet, Gold, Gold_Sheet;
     public static void load(){
         Iron = new Item("iron"){{
             hardness = 1;
             cost = 0.5f;
-            alwaysUnlocked = true;
+            alwaysUnlocked = false;
         }};
         Iron_Sheet = new Item("iron_sheet"){{
             hardness = 1;
-            cost = 0.6f;
+            cost = 0.5f;
+            alwaysUnlocked = false;
+        }};
+        Gold = new Item("gold"){{
+            hardness = 1;
+            cost = 0.5f;
+            alwaysUnlocked = false;
+        }};
+        Gold_Sheet = new Item("gold_sheet"){{
+            hardness = 1;
+            cost = 0.5f;
             alwaysUnlocked = false;
         }};
     }
