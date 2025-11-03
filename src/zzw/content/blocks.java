24b25e95387be9380bbf7cfb96bbe07b90465952
ZZW-vs <Zzw13675816057@outlook.com>
2025-11-03 23:08:35 +0800

    p

--- src/zzw/content/blocks.java
+++ src/zzw/content/blocks.java
@@ -5,10 +5,14 @@ import mindustry.content.Items;
 import mindustry.type.Category;
 import mindustry.type.ItemStack;
 import mindustry.world.Block;
+import mindustry.world.blocks.distribution.Conveyor;
 import mindustry.world.blocks.production.GenericCrafter;
 
+import static mindustry.type.ItemStack.with;
+
+
 public class blocks {
-    public static Block Plate_Maker_Iron, Large_Plate_Maker_Iron, Plate_Maker_Gold, Large_Plate_Maker_Gold;
+    public static Block Plate_Maker_Iron, Large_Plate_Maker_Iron, Plate_Maker_Gold, Large_Plate_Maker_Gold, Conveyor;
     public static void load(){
         Plate_Maker_Iron = new GenericCrafter("plate_maker_iron"){{
             requirements(Category.crafting, ItemStack.with(Items.copper, 90, Items.lead, 70, items.Iron, 35));
@@ -45,6 +49,8 @@ public class blocks {
             outputItem = new ItemStack(items.Gold_Sheet, 2);
             consumeItem(items.Gold, 2);
 
+            hasPower = true;
+
             size = 2;
             hasItems = true;
             craftTime = 80f;
@@ -61,6 +67,15 @@ public class blocks {
             size = 3;
             hasItems = true;
             craftTime = 70f;
+        }};
+
+        Conveyor = new Conveyor("conveyor_zzw"){{
+            requirements(Category.distribution, with(items.Iron, 1));
+            health = 55;
+            speed = 0.5f;
+            displayedSpeed = 4.2f;
+            buildCostMultiplier = 2f;
+            researchCost = with(items.Iron, 5);
         }};
 
     }
