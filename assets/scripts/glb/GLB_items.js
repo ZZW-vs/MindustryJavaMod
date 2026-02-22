7d2492e7278dd5a0defcd17caa84cc70d95b4939
ZZW-vs <Zzw13675816057@outlook.com>
2026-02-22 17:58:32 +0800

    1.2.2

--- assets/scripts/glb/GLB_items.js
+++ assets/scripts/glb/GLB_items.js
@@ -2,27 +2,31 @@ module.exports = {
   load() {
     Log.info("[ZZW] Referencing items from Java...");
 
-    zzw.content.items.Iron = Vars.content.item("iron");
-    zzw.content.items.Gold = Vars.content.item("gold");
-    zzw.content.items.Andesite = Vars.content.item("andesite");
-    zzw.content.items.Andesite_Alloy = Vars.content.item("andesite_alloy");
-    zzw.content.items.Brass = Vars.content.item("brass");
-    zzw.content.items.Zinc = Vars.content.item("zinc");
+    zzw.content.items.Iron = JSBridge.getItem("iron");
+    zzw.content.items.Gold = JSBridge.getItem("gold");
+    zzw.content.items.Andesite = JSBridge.getItem("andesite");
+    zzw.content.items.Andesite_Alloy = JSBridge.getItem("andesite_alloy");
+    zzw.content.items.Brass = JSBridge.getItem("brass");
+    zzw.content.items.Zinc = JSBridge.getItem("zinc");
 
-    zzw.content.items.Iron_Sheet = Vars.content.item("iron_sheet");
-    zzw.content.items.Gold_Sheet = Vars.content.item("gold_sheet");
-    zzw.content.items.Copper_Sheet = Vars.content.item("copper_sheet");
-    zzw.content.items.Brass_Sheet = Vars.content.item("brass_sheet");
+    zzw.content.items.Iron_Sheet = JSBridge.getItem("iron_sheet");
+    zzw.content.items.Gold_Sheet = JSBridge.getItem("gold_sheet");
+    zzw.content.items.Copper_Sheet = JSBridge.getItem("copper_sheet");
+    zzw.content.items.Brass_Sheet = JSBridge.getItem("brass_sheet");
 
-    zzw.content.items.Pumpkin_Seeds = Vars.content.item("pumpkin_seeds");
-    zzw.content.items.Pulp = Vars.content.item("pulp");
+    zzw.content.items.Pumpkin_Seeds = JSBridge.getItem("pumpkin_seeds");
+    zzw.content.items.Pulp = JSBridge.getItem("pulp");
 
-    zzw.content.items.Text_An = Vars.content.item("campfire_fire");
+    zzw.content.items.Text_An = JSBridge.getItem("campfire_fire");
 
     Log.info("[ZZW] Items referenced from Java");
   },
 
   getItem(name) {
-    return zzw.content.items[name];
+    const item = zzw.content.items[name];
+    if (!item) {
+      Log.warn("[ZZW] Item not found: " + name);
+    }
+    return item;
   }
 };
