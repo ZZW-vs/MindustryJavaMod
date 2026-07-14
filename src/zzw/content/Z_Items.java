4d119503890d72f72c07a7c6380898679270a533
ZZW-vs <zhengzip@example.com>
2026-07-14 23:01:04 +0800

    任务13: 移植PU132所有物品
    - 修改 Stone 为 PU132 原版属性
    - 新增 24 个 PU132 物品 (advance-alloy, cupronickel, dark-alloy, dirium, light-alloy, monolith-alloy, archaic-debris, plague-alloy, spark-alloy, super-alloy, termina-alloy, termination-fragment, terminum, contagium, dense-alloy, imberium, irradiant-surge, luminum, monolite, nickel, steel, umbrium, xenium, uranium)
    - 所有属性完全照搬 PU132 原版 (cost/radioactivity/hardness/flammability/explosiveness/lowPriority)
    - AnimatedItem 用普通 Item + frames/frameTime/transitionFrames 模拟
    - 复制 45 个贴图到 assets/sprites/items/
    - 更新 bundle.properties 和 bundle_zh_CN.properties (25 个物品翻译)

--- src/zzw/content/Z_Items.java
+++ src/zzw/content/Z_Items.java
@@ -4,8 +4,12 @@ import arc.graphics.Color;
 import mindustry.type.Item;
 
 public class Z_Items {
-    //ProjectUnity物品
-    public static Item Stone;
+    // ProjectUnity物品 (faction-alloys)
+    public static Item advanceAlloy, cupronickel, darkAlloy, dirium, lightAlloy, monolithAlloy, archDebris,
+            plagueAlloy, sparkAlloy, superAlloy, terminaAlloy, terminationFragment, terminum;
+    // ProjectUnity物品 (faction items)
+    public static Item contagium, denseAlloy, imberium, irradiantSurge, luminum, monolite, nickel, steel, stone,
+            umbrium, xenium, uranium;
     // 基础金属
     public static Item Iron, Gold, Andesite, Andesite_Alloy, Brass, Zinc;
     // 金属板材
@@ -16,8 +20,152 @@ public class Z_Items {
     public static Item Campfire_Fire;
 
     public static void load() {
-        //ProjectUnity物品
-        Stone = basic("stone", "3d4547");
+        //region ProjectUnity faction-alloys
+        advanceAlloy = new Item("advance-alloy", Color.valueOf("748096")) {{
+            cost = 1.4f;
+            radioactivity = 0.1f;
+        }};
+
+        cupronickel = new Item("cupronickel", Color.valueOf("a19975")) {{
+            cost = 2f;
+        }};
+
+        darkAlloy = new Item("dark-alloy", Color.valueOf("716264")) {{
+            cost = 1.4f;
+            radioactivity = 0.11f;
+        }};
+
+        dirium = new Item("dirium", Color.valueOf("96f7c3")) {{
+            cost = 0.3f;
+            hardness = 9;
+        }};
+
+        lightAlloy = new Item("light-alloy", Color.valueOf("e0ecee")) {{
+            cost = 1.4f;
+            radioactivity = 0.08f;
+        }};
+
+        // AnimatedItem 用 Item + frames/transitionFrames/frameTime 模拟
+        monolithAlloy = new Item("monolith-alloy", Color.valueOf("b3b3b3")) {{
+            cost = 1.4f;
+            flammability = 0.1f;
+            radioactivity = 0.12f;
+            frames = 14;
+            frameTime = 1f;
+            transitionFrames = 3;
+        }};
+
+        archDebris = new Item("archaic-debris", Color.valueOf("4d4d4d")) {{
+            cost = 1.3f;
+            radioactivity = 0.1f;
+            frames = 7;
+            frameTime = 3f;
+            transitionFrames = 1;
+        }};
+
+        plagueAlloy = new Item("plague-alloy", Color.valueOf("6a766a")) {{
+            cost = 1.4f;
+            radioactivity = 0.16f;
+        }};
+
+        sparkAlloy = new Item("spark-alloy", Color.valueOf("f4ff61")) {{
+            cost = 1.3f;
+            radioactivity = 0.01f;
+            explosiveness = 0.1f;
+        }};
+
+        superAlloy = new Item("super-alloy", Color.valueOf("67a8a0")) {{
+            cost = 2.5f;
+        }};
+
+        terminaAlloy = new Item("termina-alloy", Color.valueOf("9e6d74")) {{
+            cost = 4.2f;
+            radioactivity = 1.74f;
+        }};
+
+        terminationFragment = new Item("termination-fragment", Color.valueOf("f9504f")) {{
+            cost = 1.2f;
+            radioactivity = 3.64f;
+        }};
+
+        terminum = new Item("terminum", Color.valueOf("f53036")) {{
+            cost = 3.2f;
+            radioactivity = 1.32f;
+        }};
+        //endregion
+
+        //region ProjectUnity faction items
+        contagium = new Item("contagium", Color.valueOf("68985e")) {{
+            cost = 1.5f;
+            hardness = 3;
+            radioactivity = 0.7f;
+        }};
+
+        denseAlloy = new Item("dense-alloy", Color.valueOf("a68a84")) {{
+            hardness = 2;
+            cost = 2f;
+        }};
+
+        imberium = new Item("imberium", Color.valueOf("f6ff7d")) {{
+            cost = 1.4f;
+            hardness = 3;
+            radioactivity = 0.6f;
+        }};
+
+        irradiantSurge = new Item("irradiant-surge", Color.valueOf("3d423e")) {{
+            cost = 2f;
+            frames = 2;
+            frameTime = 3f;
+            transitionFrames = 30;
+        }};
+
+        luminum = new Item("luminum", Color.valueOf("e9eaf1")) {{
+            cost = 1.2f;
+            hardness = 3;
+            radioactivity = 0.1f;
+        }};
+
+        monolite = new Item("monolite", Color.valueOf("4d4d4d")) {{
+            cost = 1.5f;
+            hardness = 3;
+            radioactivity = 0.2f;
+            flammability = 0.2f;
+        }};
+
+        nickel = new Item("nickel", Color.valueOf("6e9675")) {{
+            hardness = 3;
+            cost = 2.5f;
+        }};
+
+        steel = new Item("steel", Color.valueOf("e1e3ed")) {{
+            hardness = 4;
+            cost = 2.5f;
+        }};
+
+        stone = new Item("stone", Color.valueOf("8a8a8a")) {{
+            hardness = 1;
+            cost = 0.4f;
+            lowPriority = true;
+        }};
+
+        umbrium = new Item("umbrium", Color.valueOf("8c3d3b")) {{
+            cost = 1.2f;
+            hardness = 3;
+            radioactivity = 0.2f;
+        }};
+
+        xenium = new Item("xenium", Color.valueOf("9dddff")) {{
+            cost = 1.2f;
+            hardness = 3;
+            radioactivity = 0.6f;
+        }};
+
+        uranium = new Item("uranium", Color.valueOf("ace284")) {{
+            cost = 2f;
+            hardness = 3;
+            radioactivity = 1f;
+        }};
+        //endregion
 
         // 基础金属
         Iron = basic("iron", "cfcfcf");
